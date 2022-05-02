e7c0f70014e2d52deb000b01fda76cc792e91a1c
Soezz <104770230+Soezz@users.noreply.github.com>
2022-05-02 19:35:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
+  ['6281386835493'],
   ['0'],
-  ['6281224863098', 'Della', true]
+  ['6281386835493', 'Soezz', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
